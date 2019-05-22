6940f3fcceaa015155e22c30120718ac75fcc7c2
unknown <niek.slemmer@hotmail.com>
2019-05-22 14:22:26 +0200

    transfer

--- Homework/Week_6/visualize.js
+++ Homework/Week_6/visualize.js
@@ -1,33 +1,31 @@
 window.onload = function() {
-  var year = "2000";
-  var fileName = 'Data/ghg_data.json';
-  var txtFile = new XMLHttpRequest();
-  txtFile.onreadystatechange = () => {
-    if (txtFile.readyState === 4 && txtFile.status == 200) {
-      main(preprocess(txtFile.responseText));
-    }
-  }
-  txtFile.open("GET", fileName);
-  txtFile.send();
+  main();
 };
 
-var main = function(year) {
-
-}
+var main = async function() {
+  var year = "2010";
+  var fileName = "Data/ghg_data.json";
+  country_data = await fetch(fileName)
+    .then((response) => {
+      return response.json();
+    });
+};
 
 var preprocess = function(data, year) {
   json_data = JSON.parse(data);
-  console.log(json_data);
+  countries = [];
   for (let i = 0; i < json_data.length; i++) {
-    categories = [];
-    country_name = json_data[i]["country_or_area"];
+    country_name = json_data[i].country_or_area;
     let country = {
       name:country_name
     };
     i++;
-    while (i < json_data.length || json_data[i]["country_or_area"] == country_name) {
-      eval("country." + Object.keys(json_data[i])[3] + ":" + json_data[i]["value"]);
+    while (i < json_data.length || json_data[i].country_or_area == country_name) {
+      eval("country." + Object.keys(json_data[i])[3] + ":" + json_data[i].value);
+      i++;
     }
+
+
   }
   return json_data;
-}
+};
